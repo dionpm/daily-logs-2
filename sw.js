7ca61304b41c57d5b247c24e57c6db42a5f95592
dionpm <dionmobley@gmail.com>
2025-08-10 22:01:07 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service worker (v3) to bust cache after updates
-const CACHE_NAME = 'dtl-cache-v3';
+// Service worker (v4) to bust cache after updates
+const CACHE_NAME = 'dtl-cache-v4';
 const APP_SHELL = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', (event) => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))); self.skipWaiting(); });
 self.addEventListener('activate', (event) => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))); self.clients.claim(); });
