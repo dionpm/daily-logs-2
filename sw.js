0d07401336daea742ea151423c51b15119ec0d39
dionpm <dionmobley@gmail.com>
2025-08-10 21:45:46 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,47 +1,13 @@
-// Service worker (v2) with cache-busting
-const CACHE_NAME = 'dtl-cache-v2';
-const APP_SHELL = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-    ))
-  );
-  self.clients.claim();
-});
-
+// Service worker (v3) to bust cache after updates
+const CACHE_NAME = 'dtl-cache-v3';
+const APP_SHELL = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', (event) => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))); self.skipWaiting(); });
+self.addEventListener('activate', (event) => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))); self.clients.claim(); });
 self.addEventListener('fetch', (event) => {
-  const req = event.request;
-  const url = new URL(req.url);
+  const req = event.request; const url = new URL(req.url);
   if (req.destination === 'document' || url.pathname.endsWith('/')) {
-    event.respondWith(
-      fetch(req).then((res) => {
-        const copy = res.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
-        return res;
-      }).catch(() => caches.match(req))
-    );
+    event.respondWith(fetch(req).then(res => { caches.open(CACHE_NAME).then(c=>c.put(req,res.clone())); return res; }).catch(()=>caches.match(req)));
   } else {
-    event.respondWith(
-      caches.match(req).then((cached) => cached || fetch(req).then((res) => {
-        const copy = res.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
-        return res;
-      }))
-    );
+    event.respondWith(caches.match(req).then(cached => cached || fetch(req).then(res => { caches.open(CACHE_NAME).then(c=>c.put(req,res.clone())); return res; })));
   }
 });
